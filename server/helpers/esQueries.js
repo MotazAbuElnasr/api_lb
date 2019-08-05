44995baa70908b74e3e5f05476340457eb623200
Motaz Abu Elnasr <motaz.abuelnasr@gmail.com>
2019-08-05 18:07:31 +0200

    add auth to elastic search

--- server/helpers/esQueries.js
+++ server/helpers/esQueries.js
@@ -1,7 +1,11 @@
 const { Client } = require("@elastic/elasticsearch");
 
 const client = new Client({
-  node: process.env.ELASTIC_URL
+  node: process.env.ELASTIC_URL,
+  auth: {
+    username: process.env.ELASTIC_USER,
+    password: process.env.ELASTIC_PASSWORD
+  }
 });
 
 const searchUsers = async user => {
